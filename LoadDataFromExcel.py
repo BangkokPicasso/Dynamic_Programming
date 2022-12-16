8104c5772cf8949bbeadc9022e02b9d7d59b7a9e
BangkokPicasso <6161qeqe@gmail.com>
2022-12-16 11:26:22 +0800

    init

---
+++ LoadDataFromExcel.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os.path
+
+
+
+File_Path = f"D:\\ALL\\參考\\\Dynamic Programming\\DP2.xlsx"
+
+if os.path.isfile(File_Path):
+    print("  File Found  " + File_Path)
+    df = pd.read_excel(File_Path, sheet_name = "DP").to_dict('list')
+    print(df)
+
+else:
+    print("404 NOT FOUND")                     
+
+
+
+
+
+
