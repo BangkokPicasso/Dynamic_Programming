7110c579eabf76a2a9fb0295ee20ed97f7a75892
BangkokPicasso <6161qeqe@gmail.com>
2023-01-05 16:43:43 +0800

    [Done] IsVe & PPT

--- HW.py
+++ HW.py
@@ -16,21 +16,22 @@ import pandas as pd
 # 最少必須保留剩下餐點裡，剩餘天數個最低熱量餐點的熱量總和卡
 
 
-Dish = {'餐點': ['叻沙牛肉麵', '石鍋拌飯', '花生雞腿堡', '原汁牛肉麵', '炸豬排飯', '咖哩飯', '雞胸肉餐', '雙層牛肉吉士堡', '涼麵', '魷魚羹', '功夫麵', '烤雞排'], 
+Dish = {'餐點': ['叻沙牛肉麵', '我是素食', '神奇沙拉', '原汁牛肉麵', '炸豬排飯', '咖哩飯', '雞胸肉餐', '素肉全餐', '涼麵(素)', '魷魚羹', '功夫麵', '烤雞排'], 
 '熱量': [945, 875, 716, 677, 570, 550, 450, 440, 380, 370, 300, 300],
 '價格': [160, 140, 129, 115, 110, 120, 135, 105, 60, 70, 65, 60], 
 '喜愛程度': [100, 88, 76, 74, 71, 69, 68, 65, 62, 59, 53, 50], 
 '整體滿意度': [90, 79, 70, 68, 67, 65, 66, 63, 61, 58, 53, 50], 
-'熱量(正規後).1': [1.0, 0.8914728682170543, 0.6449612403100775, 0.5844961240310077, 0.4186046511627907, 0.3875968992248062, 0.23255813953488372, 0.21705426356589147, 0.12403100775193798, 0.10852713178294573, 0.0, 0.0]}
+'葷素': ['葷', '素', '素', '葷', '葷', '葷', '葷', '素', '素', '葷', '葷', '葷']}
 
 
 T = 7 # 總期數
 Sn = 800 # 總餐費(預算上限)
 Sc = 4000 # 總熱量(熱量上限)
 S = {} #裝各期各x下的value
-
+IsVe = "葷" # 先裝葷食
+VeDays = 3 # 吃素天數
 Dish = pd.DataFrame(Dish) # 丟進DF方便篩選
-
+Opt = {} 
 for t in range(T, 0, -1): # 建立字典中的字典，每個小字典裝當期值
     S[t] = {}
     for i in range(0 , len(Dish)): 
@@ -41,21 +42,25 @@ for t in range(T, 0, -1): # 建立字典中的字典，每個小字典裝當期�
 # {3期數: {0上一期: {0這一期: 
 for t in range(T, 0, -1):
     if t == T:
-        for i in range(0, len(Dish)): 
-            S[t][i][0]['餐點'] = Dish['餐點'][i]
-            S[t][i][0]['整體滿意度'] = Dish['整體滿意度'][i]
-            S[t][i][0]['價格'] = Dish['價格'][i]
-            S[t][i][0]['熱量'] = Dish['熱量'][i]
+        Dish2 = Dish[Dish['葷素'] == IsVe].reset_index()
+        for i in range(0, len(Dish2)): 
+            S[t][i][0]['餐點'] = Dish2['餐點'][i]
+            S[t][i][0]['整體滿意度'] = Dish2['整體滿意度'][i]
+            S[t][i][0]['價格'] = Dish2['價格'][i]
+            S[t][i][0]['熱量'] = Dish2['熱量'][i]
     else:
         DishList = []
+        if t <= VeDays: # 是否要開始裝素食
+            IsVe = "素" # 後裝素食
         for I in range(len(S[t + 1])): # 上一期的餐點組合數
             for i in range(len(S[t + 1][I])):  
                 LastDish = sorted(S[t+1][I]['餐點'].split(','))
                 Money = int(Dish[(~Dish['價格'].isin(LastDish))].sort_values(by = '價格')[['價格']][:t-1].sum()) # 最低飯錢 
                 Car = int(Dish[(~Dish['熱量'].isin(LastDish))].sort_values(by = '熱量')[['熱量']][:t-1].sum()) # 最低熱量 
                 Dish2 = Dish[(~Dish['餐點'].isin(LastDish)) & 
+                    (Dish['葷素'] == IsVe) & 
                     (Dish['價格'] <= Sn - Money - S[t + 1][I]['價格']) & 
-                    (Dish['熱量'] <= Sc - Car - S[t + 1][I]['熱量'])].reset_index()# 濾掉重複、超出預算、超出熱量餐點
+                    (Dish['熱量'] <= Sc - Car - S[t + 1][I]['熱量'])].reset_index()# 濾掉重複、超出預算、超出熱量餐點、葷或素
                 Max = []
                 order = 0
                 for a in range(0, len(Dish2)): # 這一期的餐點組合數
@@ -79,14 +84,14 @@ for t in range(T, 0, -1):
                 del S[t][I][i] # 清掉多餘dict
         S[t][I] = MaxS  # 只留最大值組合
 
-print(S)
-print("")
-Opt = {} 
-for t in range(T, 0, -1):
-    MaxS = S[t][0]
+    for i in range(len(S[t])):
+        if len(S[t][i]):
+            MaxS = S[t][i]
+            break
     Order = 0
     for i in range(len(S[t])):
         if len(S[t][i]):
+            
             if S[t][i]['整體滿意度'] >= MaxS['整體滿意度']:
                 MaxS = S[t][i] 
                 Opt[t] = {Order: MaxS}
--- XlsxtoDF.py
+++ XlsxtoDF.py
@@ -2,8 +2,7 @@ import pandas as pd
 import os.path
 
 
-
-File_Path = f"D:\\User\\Desktop\\Dynamic_Programming\\DP2.xlsx"
+File_Path = "D:\\ALL\\參考\\Dynamic Programming\\DP2.xlsx"
 
 if os.path.isfile(File_Path):
     print("  File Found  " + File_Path)
@@ -12,12 +11,3 @@ if os.path.isfile(File_Path):
 
 else:
     print("404 NOT FOUND")         
-
-
-
-{0: {'餐點': '功夫麵,原汁牛肉麵,叻沙牛肉麵', '整體滿意度': 211, '價格': 340}, 
-1: {'餐點': '魷魚羹,花生雞腿堡,石鍋拌飯', '整體滿意度': 207, '價格': 339}, 
-2: {'餐點': '魷魚羹,石鍋拌飯,花生雞腿堡', '整體滿意度': 207, '價格': 339}, 
-3: {'餐點': '功夫麵,叻沙牛肉麵,原汁牛肉麵', '整體滿意度': 211, '價格': 340}, 
-4: {'餐點': '功夫麵,叻沙牛肉麵,魷魚羹', '整體滿意度': 201, '價格': 295}, 
-5: {'餐點': '原汁牛肉麵,叻沙牛肉麵,功夫麵', '整體 滿意度': 211, '價格': 340}}
